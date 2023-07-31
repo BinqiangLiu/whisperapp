db846a46035b15ae362f42ff087b92e15e4bf395
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-07-31 12:35:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,16 @@ import whisper
 import subprocess
 import openai
 import ffmpeg
+import soundfile as sf
+import numpy as np
+#import av
+#from pydub import AudioSegment
+#import pyaudio
+import wave
+import sys
+# Load environment variables
+from dotenv import load_dotenv
+import os
 
 model = whisper.load_model("base")
 audio = "audio.mp3"
